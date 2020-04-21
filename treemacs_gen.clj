b72e91186bea32ab404194ed41f8a906a755d83f
shohi <oshohi@gmail.com>
2020-04-21 21:49:17 +0800

    update

--- treemacs_gen.clj
+++ treemacs_gen.clj
@@ -2,28 +2,29 @@
 
 (defn is-matched-dir?
   [ptn file]
-  (def p (re-pattern ptn))
-  (and (.isDirectory file) (re-find p (.getName file))))
+  (let [p (re-pattern ptn)]
+    (and (.isDirectory file)
+         (re-find p (.getName file)))))
 
 (defn project-item
   [file]
-  (def abs-path (.getCanonicalPath file))
-  (def name (.getName file))
-  (format "** %s\n - path :: %s" name abs-path))
+  (let [abs-path (.getCanonicalPath file)
+        name (.getName file)]
+    (format "** %s\n - path :: %s" name abs-path)))
 
 (defn create-project-items
   [dir ptn]
-  (def files (.listFiles (io/file dir)))
-  (-> files
+  (let [files (.listFiles (io/file dir))]
+    (-> files
       (#(filter (partial is-matched-dir? ptn) %))
       (#(map project-item %))
-      (#(clojure.string/join "\n" %))))
+      (#(clojure.string/join "\n" %)))))
 
 (defn create-project-list
   "create treemacs workspace project lists"
   [ws dir ptn]
-  (def header (format "* %s" ws))
-  (str header "\n" (create-project-items dir ptn)))
+  (let [header (format "* %s" ws)]
+    (str header "\n" (create-project-items dir ptn))))
 
 ;; main entry
 (let  [[ws dir ptn] *command-line-args*]
