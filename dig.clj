c5860f8ad0d7dc49538682546dbeb3f9b0288040
shohi <oshohi@gmail.com>
2020-04-30 18:57:45 +0800

    update

--- dig.clj
+++ dig.clj
@@ -3,13 +3,22 @@
 ;; TODO
 ;; +short only show A records
 (def c (:out (shell/sh "dig" "+short" "@1.0.0.1" "yahoo.com")))
-
 (def ip-pattern #"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+
+
+(defn get-ip
+  "domain ip look up and return host record"
+  [dns domain]
+  (let [c (:out (shell/sh "dig" "+short" "@1.0.0.1" "yahoo.com"))]
+    (if (empty? c))
+    )
+  )
 
 (comment
 
+  (re-matches ip-pattern c)
   ;; match
-  (re-matches ip-patern "1.0.0.1")
+  (re-matches ip-pattern "1.0.0.1")
 
   ;; not match
   (re-matches ip-pattern "1.0.0.1.")
