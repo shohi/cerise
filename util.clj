b49ba7f0a0783b846ea508a01f4c69fd5d231bc7
shohi <oshohi@gmail.com>
2020-07-29 19:38:15 +0800

    update

--- util.clj
+++ util.clj
@@ -1,22 +1,37 @@
 #!/usr/bin/env bb
 
+(use 'clojure.data)
+
 (def ignore-list ["baidunetdisk"])
 
 ;; TODO: check shell status
-(defn get-outdated-list
+(defn get-brew-outdated-list
   "get outdated brew list"
   []
-  (let [out (:out (apply shell/sh ["brew" "outdated"]))]
+  (let [out (:out (shell/sh "brew" "outdated"))]
     (str/split out #"\n")
     ))
 
-(defn get-update-list
-  "update list excluding the ones in ignore list"
+(defn get-brew-upgrade-list
+  "get upgrade list excluding the ones in ignore list"
   []
-  (let [all (get-outdated-list)]
-    ;; TODO: return diff
-    )
-  )
+  (let [all (get-brew-outdated-list)
+        items (first (diff all ignore-list))]
+
+    (into [] (remove nil? items))))
+
+(defn do-brew-upgrade
+  "run brew upgrade"
+  []
+  (let [items (get-brew-upgrade-list)]
+    (do
+      (println "upgrade brew:" items)
+
+      (for [t items]
+        (do
+          (println "brew upgrade" t)
+          (println (:out (shell/sh "brew" "upgrade" t)))
+          (println "brew upgrade done!"))))))
 
 ;; TODO: check shell status
 (defn daily-routine
@@ -24,11 +39,20 @@
   - brew update & brew upgrade
   "
   []
-  )
+  (do
+    (do-brew-upgrade)
+    ))
+
+(defn main
+  "main entry"
+  [args]
+  (daily-routine))
+
+(main *command-line-args*)
 
 (comment
   (prn (:out (apply shell/sh ["brew" "update"])))
-  (prn (get-outdated-list))
+  (prn (get-brew-upgrade-list))
   (prn (:out (shell/sh "brew" "outdated")))
   (println "hello\n")
  )
